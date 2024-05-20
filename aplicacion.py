321a60a677068a877b7602eba9c3ee9ae437737c
Zuñiga <stefanyyesenia811@gmail.com>
2024-05-20 15:21:21 -0500

    vamos

---
+++ aplicacion.py
@@ -0,0 +1,27 @@
+
+def validar_credenciales(usuario, clave):
+    with open("usuario.txt", "r") as file_usuario, open("claves.txt", "r") as file_claves:
+        logins = file_usuario.read().splitlines()
+        claves = file_claves.read().splitlines()
+        # Verificar si el usuario y la clave coinciden
+        if usuario in logins and clave == claves[logins.index(usuario)]:
+            return True
+        else:
+            return False
+
+intentos = 0
+while intentos < 2:
+    usuario = input("Ingrese su login: ")
+    clave = input("Ingrese su clave: ")
+    if validar_credenciales(usuario, clave):
+        print("Bienvenido al programa.")
+        print("Menú:")
+        print("1. Listar personas")
+        print("2. Agregar personas")
+        print("3. Salir")
+        break
+    else:
+        print("Login o clave incorrectos. Intente nuevamente.")
+        intentos += 1
+else:
+    print("Ha excedido el número de intentos permitidos. El programa terminará.")
